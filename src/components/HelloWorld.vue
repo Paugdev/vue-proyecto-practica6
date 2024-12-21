2f67b973963c0d78054f956602dd09d3611e1449
Paugdev <paulina.gonzalezx@gmail.com>
2024-12-21 02:50:06 -0300

    primer commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+
+  <div id="divHelloWorld">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+#divHelloWorld{
+  padding: 2%;
+  background-color: rgb(105, 0, 128);
+  color: white;
+  margin:2%;
+}
+</style>
